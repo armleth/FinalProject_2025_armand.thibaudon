f8823eb8f9e85fcd806399f9f379f334fcf28c11
Armleth <armand.thibaudon@epita.fr>
2025-05-13 18:55:42 +0200

    feat: flake + envrc

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rs-python = pkgs.python312.withPackages (
+          ps: with ps; [
+            pandas
+            numpy
+            matplotlib
+            seaborn
+          ]
+        );
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rs-python
+          ];
+        };
+      }
+    );
+}
